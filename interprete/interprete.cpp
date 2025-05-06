c1fc1b2de50acbcd2d14f1e0203bb33bfb85d6a4
Angelo Ceciliano Ortega <fabianceciliano@estudiantec.cr>
2025-05-05 20:56:00 -0600

    interprete funcional, sin testear a fondo.

--- interprete/interprete.cpp
+++ interprete/interprete.cpp
@@ -4,12 +4,8 @@
 #include <cstring>
 
 struct instruction {
-    int opcode;
-    // 0: WRITE_MEM, 1: READ_MEM, etc.
-    int num_pe;
-    // 0 – 7. Numero de núcleo
-    uint8_t src = 0xFF;
-    // default: inválido
+    int opcode;// 0: WRITE_MEM, 1: READ_MEM, etc.
+    uint8_t src = 0xFF; // num del PE
     uint64_t addr = 0;
     uint32_t size = 0;
     uint32_t num_of_cache_lines = 0;
@@ -39,7 +35,7 @@ int analicer_opcode(char *text){
         text_op[i] = text[i]; 
         i++;
         }
-        //text_op [i] = '\0';
+        text_op [i] = '\0';
 
         for(int j = 0; j < op_num ; j++ ) //Vericacion de que OPCODE tiene la linea
 
@@ -48,6 +44,50 @@ int analicer_opcode(char *text){
         }
 
         return EXIT_FAILURE;
+}
+
+
+void parse_instruction(char* line, instruction* inst){
+    char* token = strtok(line," \n"); //De la linea saca la primer palabra, es decir la funcion (op)
+    if(!token) return; //Verificar que hay opcode
+    inst -> opcode = analicer_opcode(line); //Indentificar el opcode
+    token = strtok(nullptr, ", \n"); //Declarando en el token el resto de parametos de la linea
+
+    //Como ya sabemos que instruccion es ya sabemos que parametros usara 
+
+    switch (inst->opcode)
+    {
+    case 0: // WRITE_MEM 
+        if (token) { inst->addr = strtoull(token, nullptr, 0);       token = strtok(nullptr, ", \n"); }
+        if (token) { inst->num_of_cache_lines = (uint32_t)strtoul(token, nullptr, 0);        token = strtok(nullptr, ", \n"); }
+        if (token) { inst->start_cache_line = (uint32_t)strtoul(token,nullptr,0);   token = strtok(nullptr, ", \n");}
+        break;
+
+    case 1: // READ_MEM 
+        if (token) { inst->addr = strtoull(token, nullptr, 0);       token = strtok(nullptr, ", \n"); }
+        if (token) { inst->size = (uint32_t)strtoul(token, nullptr, 0); token = strtok(nullptr, ", \n"); }
+        if (!token) {inst->size = 4;} //Si no se especifica el size 
+        break;
+    case 2:
+        if (token) { inst->cache_line = (uint32_t)strtoul(token,nullptr,0);   token = strtok(nullptr, ", \n");}
+        break;
+    
+    case 4://INV COMPLETE
+        if (token) { inst->dest = (uint8_t)strtoul(token,nullptr,0);   token = strtok(nullptr, ", \n");}
+        break;
+    case 5://READ_RESP
+        if (token) { inst->dest = (uint8_t)strtoul(token,nullptr,0);   token = strtok(nullptr, ", \n");}
+        if (token) { inst->data = (uint32_t)strtoul(token,nullptr,0);   token = strtok(nullptr, ", \n");}
+    case 6://WRITE_RESP
+        if (token) { inst->dest = (uint8_t)strtoul(token,nullptr,0);   token = strtok(nullptr, ", \n");}
+        if (token) { inst->status = (uint8_t)strtoul(token,nullptr,0);   token = strtok(nullptr, ", \n");}
+
+    default:
+        break;
+    }
+
+
+
 }
 
 
@@ -67,12 +107,30 @@ instruction *interpretate(const char* path){
     }
     
 
+    uint8_t current_src = 0xFF;
+    uint8_t current_qos = 0xFF;
     int i = 0;
     char line[256];//Buffer del tamano de la linea 
     while (fgets(line,sizeof(line),fp))
     {
+        if(line[0] == '\n' || line[0] == '\0' ) continue;//ignorar lineas en vacias
         printf("Analizando: %s \n",line);
-        instructions[i].opcode = analicer_opcode(line);
+        
+        if (strncmp(line, "PE", 2) == 0) {//Declarar el PE y qos para todas la lineas
+            int pe_num;
+            uint8_t qos;
+            if (sscanf(line, "PE%d %hhx", &pe_num, &qos) == 2) {
+                current_src = (uint8_t)pe_num;
+                current_qos = qos;
+            } else {
+                fprintf(stderr, "Formato PE inválido: %s", line);
+            }
+            continue;
+        }
+        instructions[i].src = current_src;
+        instructions[i].qos = current_qos;
+
+        parse_instruction(line,&instructions[i]);
         i++;
         
     }
@@ -87,13 +145,29 @@ instruction *interpretate(const char* path){
 
 int main(){
 
-    int count = 32;
+    int count = 16;
 
     instruction* program = interpretate("code_test.txt");
 
     if (program) {
         for (int i = 0; i < count; ++i) {
-            printf("Opcode #%d: %d\n", i, program[i].opcode);
+           
+           printf(
+            "inst #%d: op=%d,src=0x%02X, addr=0x%lX, size=%u, lines=%u, start_line=%u, cache_line=%u, dest=0x%02X, data=%u, status=0x%02X, qos=0x%02X\n",
+            i,
+            program[i].opcode,
+            program[i].src,
+            program[i].addr,
+            program[i].size,
+            program[i].num_of_cache_lines,
+            program[i].start_cache_line,
+            program[i].cache_line,
+            program[i].dest,
+            program[i].data,
+            program[i].status,
+            program[i].qos
+        );
+        
         }
         free(program);
     }
